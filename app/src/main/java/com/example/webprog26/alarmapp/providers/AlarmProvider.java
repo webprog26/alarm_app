1eaece509bbf7ab3a6a939ded0df79fc17695d84
webprog26 <webprog26@gmail.com>
2016-10-06 11:29:56 +0300

    Alarms can be now created & deleted

--- app/src/main/java/com/example/webprog26/alarmapp/providers/AlarmProvider.java
+++ app/src/main/java/com/example/webprog26/alarmapp/providers/AlarmProvider.java
@@ -2,41 +2,11 @@ package com.example.webprog26.alarmapp.providers;
 
 import android.app.Activity;
 import android.content.ContentValues;
-<<<<<<< HEAD
-=======
 import android.database.Cursor;
->>>>>>> Alarms can be now created & deleted
 
 import com.example.webprog26.alarmapp.db.SQLiteHelper;
 import com.example.webprog26.alarmapp.models.Alarm;
 
-<<<<<<< HEAD
-/**
- * Created by webprog26 on 05.10.2016.
- */
-public class AlarmProvider {
-    private Activity mActivity;
-    private SQLiteHelper mSqLiteHelper;
-
-    public AlarmProvider(Activity mActivity) {
-        this.mActivity = mActivity;
-        this.mSqLiteHelper = new SQLiteHelper(mActivity);
-    }
-
-    public long insertAlarmToDB(Alarm alarm)
-    {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(SQLiteHelper.ALARM_HOUR, alarm.getAlarmHour());
-        contentValues.put(SQLiteHelper.ALARM_MINUTE, alarm.getAlarmMinute());
-        contentValues.put(SQLiteHelper.IS_ALARM_ACTIVE, alarm.isAlarmStateActive());
-        contentValues.put(SQLiteHelper.IS_REPEAT_ON, alarm.isRepeatStateActive());
-        contentValues.put(SQLiteHelper.IS_VIBRATION_ON, alarm.isVibrationStateActive());
-        contentValues.put(SQLiteHelper.MELODY_FILE_PATH, alarm.getFileMelodyPath());
-        contentValues.put(SQLiteHelper.MELODY_FILE_TITLE, alarm.getFileMelodyTitle());
-
-        return mSqLiteHelper.getWritableDatabase().insert(SQLiteHelper.ALARMS_TABLE_TITLE, null, contentValues);
-    }
-=======
 import java.util.LinkedList;
 import java.util.List;
 
@@ -102,5 +72,4 @@ public class AlarmProvider {
     {
         return mSqLiteHelper.getWritableDatabase().delete(SQLiteHelper.ALARMS_TABLE_TITLE, SQLiteHelper.ALARM_ID + " = " + alarmId, null);
     }
->>>>>>> Alarms can be now created & deleted
 }
